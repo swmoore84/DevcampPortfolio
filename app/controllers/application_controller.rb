359a8bcd8d62060aa0531d522f67ea38156e3471
Sam Moore <swmoore84@gmail.com>
2020-03-27 19:22:46 +0900

    added new parameters for user registration and editing account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :device_controller? #configure_permitted_parameters can be any name
+
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
