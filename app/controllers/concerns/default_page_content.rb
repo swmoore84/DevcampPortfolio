5323fd5d58e880226db83169bd3992a59994385d
Sam Moore <swmoore84@gmail.com>
2020-04-01 12:36:17 +0900

    fixed error in concern file

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -1,7 +1,9 @@
 module DefaultPageContent
   extend ActiveSupport::Concern
 
-  before_action :set_page_defaults
+  included do
+    before_action :set_page_defaults
+  end
 
   def set_page_defaults
     @page_title = "DevcampPortfolio | My Portfolio Website"
