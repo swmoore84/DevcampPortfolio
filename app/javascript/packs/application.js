282e8719000c3bf02a157db6f7881ce70a117939
Sam Moore <swmoore84@gmail.com>
2020-04-09 11:14:29 +0900

    installed jquery UI and sortable JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//= require jquery-ui
+require("packs/html.sortable.js")
 
 import 'bootstrap';
 import './src/application.scss'
 
-console.log("Hello from here");
