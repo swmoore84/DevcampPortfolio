eb4858e7e433dc449a4f6f189c62bed571a0dd5b
Sam Moore <swmoore84@gmail.com>
2020-04-09 11:27:46 +0900

    made changes to JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-//= require jquery-ui
+
+require("jquery")
+require("jquery-ui")
 require("packs/html.sortable.js")
+
+$(function(){
+  $('.sortable').sortable();
+});
+
 
 import 'bootstrap';
 import './src/application.scss'
