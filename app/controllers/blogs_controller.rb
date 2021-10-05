7c7101794343e6e519b6295817256dcc63f632a3
Sam Moore <swmoore84@gmail.com>
2021-10-05 11:07:43 +0900

    add button to toggle status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ show edit update destroy ]
+  before_action :set_blog, only: %i[show edit update destroy toggle_status]
 
   # GET /blogs or /blogs.json
   def index
@@ -50,6 +50,15 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: "Post was removed." }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    redirect_to blogs_url, notice: "Blog status was changed."
   end
 
   private
