7a5f42e1100541b0f8746fd39d3a22f485eebc30
Sam Moore <swmoore84@gmail.com>
2022-02-07 23:13:11 +0900

    add image defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,9 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails')}
 
   after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
