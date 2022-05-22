0343054b65014628cf71291f17d36e52e31c6bd3
Sam Moore <swmoore84@gmail.com>
2022-05-22 23:41:04 +0900

    move sections to partials

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My portfolio blog"
   end
 
